aa3e35fdd1b72fb249cd2b806d5507242c7adb3e
Sogo2012 <ingenieria@ecoconsultor.com>
2026-03-01 15:32:18 -0600

    Create app.py file for SkyCalc 2.0 application

---
+++ app.py
@@ -0,0 +1,13 @@
+# SkyCalc 2.0
+
+This is the main Streamlit application file for SkyCalc 2.0.
+
+import streamlit as st
+
+def main():
+    st.title('SkyCalc 2.0')
+    st.write('Welcome to SkyCalc 2.0!')
+    # Add your application logic here
+
+if __name__ == '__main__':
+    main()
